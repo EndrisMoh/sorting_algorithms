b935d4cd07bf4e9b5c0d12b8ebdf76759e8e64ed
EndrisMoh <endrismd@gmail.com>
2022-11-07 09:20:15 -0800

    Shell sort algorithm - Knuth Sequence

--- sort.h
+++ sort.h
@@ -42,4 +42,4 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 /*void sort_deck(deck_node_t **deck); */
 
-#endif /* End SORT_H */
+#endif /* SORT_H */
